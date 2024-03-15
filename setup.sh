79ae1ddd794718ef97e4980d22bf5986d83e6df2
Vicky Y. Feng <vfeng02@gmail.com>
2024-03-15 15:38:55 -0400

    env yml, setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ fi
 conda env create --name ${envname} --file=env.yml
 conda activate ${envname}
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash #install nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 cd frontend
 npm i
